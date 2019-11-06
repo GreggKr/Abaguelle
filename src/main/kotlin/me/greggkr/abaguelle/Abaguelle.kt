10ac162c79abcc796bdc4dbff54c7a8d78951c94
Grzegorz <greggkrzykowski@gmail.com>
2019-11-06 17:53:06 -0600

    dabbaguelle

--- src/main/kotlin/me/greggkr/abaguelle/Abaguelle.kt
+++ src/main/kotlin/me/greggkr/abaguelle/Abaguelle.kt
@@ -1,10 +1,11 @@
 package me.greggkr.abaguelle
 
-import me.greggkr.abaguelle.commands.InfoCommand
+import me.greggkr.abaguelle.commands.AboutCommand
+import me.greggkr.abaguelle.commands.DabbyCommand
 import me.greggkr.kdbf.startBot
 import java.io.File
 
 fun main() {
     val file = File("src/main/resources/config.json")
-    startBot(file, listOf(InfoCommand()))
+    startBot(file, listOf(AboutCommand(), DabbyCommand()))
 }
--- src/main/kotlin/me/greggkr/abaguelle/commands/DabbyCommand.kt
+++ src/main/kotlin/me/greggkr/abaguelle/commands/DabbyCommand.kt
@@ -5,14 +5,12 @@ import me.greggkr.kdbf.command.CommandData
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 
-@CommandData(
-    name = "info"
-)
-class InfoCommand : Command {
+@CommandData("dabby")
+class DabbyCommand : Command {
     override fun execute(event: MessageReceivedEvent, args: String?) {
         event.channel.sendMessage(
             EmbedBuilder()
-                .setDescription("Version 2 of the osu! discord bot Abaguelle.\nCreated by Gregg#1174")
+                .setImage("https://cdn.discordapp.com/attachments/434004583300202497/508767853416284172/dabby.png")
                 .build()
         ).queue()
     }
