5c749c046eae5786e5528d0a8d514de9fe1b5fe4
Grzegorz <greggkrzykowski@gmail.com>
2019-11-07 15:41:53 -0600

    move to fun pckg

--- src/main/kotlin/me/greggkr/abaguelle/commands/fun/BigCommand.kt
+++ src/main/kotlin/me/greggkr/abaguelle/commands/fun/BigCommand.kt
@@ -1,16 +1,25 @@
-package me.greggkr.abaguelle.commands
+package me.greggkr.abaguelle.commands.`fun`
 
 import me.greggkr.kdbf.command.Command
 import me.greggkr.kdbf.command.CommandData
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 
-@CommandData("dabby")
-class DabbyCommand : Command {
+@CommandData(
+    name = "big",
+    triggers = ["bigemoji"]
+)
+class BigCommand : Command {
     override fun execute(event: MessageReceivedEvent, args: String?) {
+        val emotes = event.message.emotes
+        if (emotes.isEmpty()) {
+            event.channel.sendMessage("Correct Usage: big <emote>").queue()
+            return
+        }
+
         event.channel.sendMessage(
             EmbedBuilder()
-                .setImage("https://cdn.discordapp.com/attachments/434004583300202497/508767853416284172/dabby.png")
+                .setImage(emotes[0].imageUrl)
                 .build()
         ).queue()
     }
--- src/main/kotlin/me/greggkr/abaguelle/commands/fun/DabbyCommand.kt
+++ src/main/kotlin/me/greggkr/abaguelle/commands/fun/DabbyCommand.kt
@@ -1,25 +1,16 @@
-package me.greggkr.abaguelle.commands
+package me.greggkr.abaguelle.commands.`fun`
 
 import me.greggkr.kdbf.command.Command
 import me.greggkr.kdbf.command.CommandData
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 
-@CommandData(
-    name = "big",
-    triggers = ["bigemoji"]
-)
-class BigCommand : Command {
+@CommandData("dabby")
+class DabbyCommand : Command {
     override fun execute(event: MessageReceivedEvent, args: String?) {
-        val emotes = event.message.emotes
-        if (emotes.isEmpty()) {
-            event.channel.sendMessage("Correct Usage: big <emote>").queue()
-            return
-        }
-
         event.channel.sendMessage(
             EmbedBuilder()
-                .setImage(emotes[0].imageUrl)
+                .setImage("https://cdn.discordapp.com/attachments/434004583300202497/508767853416284172/dabby.png")
                 .build()
         ).queue()
     }
