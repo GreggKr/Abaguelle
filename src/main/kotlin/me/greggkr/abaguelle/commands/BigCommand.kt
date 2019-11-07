baff7186b84565d0c31befc53bd07a276d7501eb
Grzegorz <greggkrzykowski@gmail.com>
2019-11-07 13:33:49 -0600

    big emotes owo

---
+++ src/main/kotlin/me/greggkr/abaguelle/commands/BigCommand.kt
@@ -0,0 +1,26 @@
+package me.greggkr.abaguelle.commands
+
+import me.greggkr.kdbf.command.Command
+import me.greggkr.kdbf.command.CommandData
+import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+
+@CommandData(
+    name = "big",
+    triggers = ["bigemoji"]
+)
+class BigCommand : Command {
+    override fun execute(event: MessageReceivedEvent, args: String?) {
+        val emotes = event.message.emotes
+        if (emotes.isEmpty()) {
+            event.channel.sendMessage("Correct Usage: big <emote>").queue()
+            return
+        }
+
+        event.channel.sendMessage(
+            EmbedBuilder()
+                .setImage(emotes[0].imageUrl)
+                .build()
+        ).queue()
+    }
+}
