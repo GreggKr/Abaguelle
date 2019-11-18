95da05b7c75fbbf04a17ddbc30cd45086e107654
Grzegorz <greggkrzykowski@gmail.com>
2019-11-18 14:46:03 -0600

    changed my mind

--- src/main/kotlin/me/greggkr/abaguelle/Abaguelle.kt
+++ src/main/kotlin/me/greggkr/abaguelle/Abaguelle.kt
@@ -1,10 +1,22 @@
 package me.greggkr.abaguelle
 
+import com.oopsjpeg.osu4j.backend.Osu
 import me.greggkr.kdbf.command.discoverCommands
+import me.greggkr.kdbf.config
 import me.greggkr.kdbf.startBot
 import java.io.File
+
+lateinit var osu: Osu
 
 fun main() {
     discoverCommands("me.greggkr.abaguelle.commands")
     startBot(File("src/main/resources/config.json"))
+
+    osu = Osu.getAPI(config["osu_token"] as String)
+
+    hideConfigValues()
+}
+
+fun hideConfigValues() {
+    config["osu_token"] = "hidden"
 }
